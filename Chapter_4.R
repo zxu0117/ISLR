5c8dc1d4b67947956edb9cbb6bb317d75f1380c4
Suzy Xu <zhuoqing.suzy.xu@gmail.com>
2020-08-14 11:10:01 -0400

    Chapter 4.6.6

--- Chapter_4.R
+++ Chapter_4.R
@@ -101,3 +101,42 @@ mean(knn_pred==Direction_2005)
 
 # train_X <- Smarket[train,c("Lag1","Lag2")]
 # test_X <- Smarket[!train,c("Lag1","Lag2")]
+
+#Applying to a different data set
+Caravan <- ISLR::Caravan
+dim(Caravan)
+summary(Caravan$Purchase)
+348/5474
+standardized_X <- scale(Caravan[,-86])
+var(Caravan[,1])
+var(Caravan[,2])
+var(standardized_X[,1])
+var(standardized_X[,2])
+test <- 1:1000
+train_X <- standardized_X[-test,]
+test_X <- standardized_X[test,]
+train_Y <- Caravan$Purchase[-test]
+test_Y <- Caravan$Purchase[test]
+set.seed(1)
+knn_pred <- knn(train_X, test_X, train_Y, k = 1)
+mean(test_Y!=knn_pred)
+mean(test_Y!="No")
+table(test_Y,knn_pred)
+knn_pred <- knn(train_X, test_X, train_Y, k = 3)
+table(knn_pred,test_Y)
+5/26
+knn_pred <- knn(train_X, test_X, train_Y, k = 5)
+table(knn_pred,test_Y)
+4/15
+
+#Fitting new data set with logistic regression
+glm_fit <-glm(Caravan$Purchase~., data = Caravan, family = binomial, subset = -test)
+glm_prob <- predict(glm_fit, Caravan[test,], type = "response")
+glm_pred <- rep("No",1000)
+glm_pred[glm_prob >0.5] <- "Yes"
+table(glm_pred,test_Y)
+glm_fit <-glm(Caravan$Purchase~., data = Caravan, family = binomial, subset = -test)
+glm_prob <- predict(glm_fit, Caravan[test,], type = "response")
+glm_pred <- rep("No",1000)
+glm_pred[glm_prob >0.25] <- "Yes"
+table(glm_pred,test_Y)
